c15969ada63a39f164e5ccf39356834cf30e5773
Luqi Pan <github@luqipan.com>
2025-10-25 22:16:07 -0700

    chore: write all games in the response of GetRecentlyPlayedGames

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const PoESteamAppID = 238960; // Path of Exile's Steam App ID
-
 export default {
   async scheduled(controller, env, ctx) {
     try {
@@ -7,19 +5,13 @@ export default {
         `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${env.STEAM_API_KEY}&steamid=${env.STEAM_ID}&format=json`
       );
       const data = await response.json();
-      const poe = data.response.games.find(
-        (item) => item.appid === PoESteamAppID
-      );
-      console.log(
-        "total play time at",
-        new Date().toISOString(),
-        poe.playtime_forever
-      );
-
-      env.STEAM_PLAYTIME_FOREVER.writeDataPoint({
-        indexes: [env.STEAM_ID],
-        blobs: [PoESteamAppID.toString()],
-        doubles: [poe.playtime_forever],
+      data.response.games.forEach((item) => {
+        env.STEAM_PLAYTIME_FOREVER.writeDataPoint({
+          indexes: [env.STEAM_ID],
+          blobs: [item.appid.toString()],
+          doubles: [item.playtime_forever],
+        });
+        console.log(item.appid, "total play time at", item.playtime_forever);
       });
     } catch (error) {
       console.error("Error:", error);
